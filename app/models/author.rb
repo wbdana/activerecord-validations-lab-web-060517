30d5db3fd93590f65588dae7ca1746fa2adf9d24
wbdana <william.b.dana@gmail.com>
2017-07-08 17:44:04 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { in: 10..10 }
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+
+  before_validation :not_clickbait
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ["Fiction","Non-Fiction"] }
+
+  def not_clickbait
+    return false if self.title.nil?
+    bad_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    bad_titles.map{|bad| self.title.include?(bad) ? true : false}.include?(true)
+  end
+
 end
